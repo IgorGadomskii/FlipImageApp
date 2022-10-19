e22ad73a8864ec4afd6f9db48514b6a61ae88881
Igor Gadomskii <igvgad@gmail.com>
2022-10-19 12:58:27 +0600

    Initial Commit

---
+++ FlipImageApp/FlipImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipImageAppApp.swift
+//  FlipImageApp
+//
+//  Created by Macbook on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlipImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
